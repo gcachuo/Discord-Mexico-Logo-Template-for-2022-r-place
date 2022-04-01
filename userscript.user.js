29804bc0747b29dd842403350119584079d29633
Cachu <gcachu.o@gmail.com>
2022-04-01 14:41:33 -0600

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         osu! Logo template
+// @name         Discord Mexico Logo template
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the canvas!
-// @author       oralekin
+// @author       gcachuo
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.mirai.gg/tmp/dotted-place-template.png";
+            i.src = "";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
